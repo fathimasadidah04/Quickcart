90227e59dff5a29a46a923dfe3c3885740b39e60
sadidah <sadidahfathima@gmail.com>
2026-03-13 12:00:25 +0530

    quickcart part1

---
+++ src/data/products.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    id: 1,
+    name: "Wireless Mouse",
+    description: "Smooth and responsive wireless mouse",
+    price: 1,
+    category: "Electronics",
+    image: "https://m.media-amazon.com/images/I/61jwB-pzZ2L._AC_SL1500_.jpg"
+  },
+  {
+    id: 2,
+    name: "Smart Watch",
+    description: "Track your fitness easily",
+    price: 2.15,
+    category: "Electronics",
+    image: "https://resources.claroshop.com/imagenes-sanborns-ii/1200/30506555599_2.jpg?scale=500&qlty=75"
+  },
+  {
+    id: 3,
+    name: "Running Shoes",
+    description: "Comfortable sports shoes",
+    price: 3,
+    category: "Fashion",
+    image: "https://www.runningxpert.com/media/wysiwyg/Artikler/2021/asics_noosa_tri_13.jpg"
+  },
+  {
+    id: 4,
+    name: "Backpack",
+    description: "Stylish travel backpack",
+    price: 4,
+    category: "Accessories",
+    image: "https://m.media-amazon.com/images/I/71UEFBe2BAL.jpg"
+  },
+  {
+    id: 5,
+    name: "Sunglasses",
+    description: "UV protection sunglasses",
+    price: 1.15,
+    category: "Fashion",
+    image: "https://img.freepik.com/premium-photo/cool-dog-with-sunglasses_849688-2918.jpg"
+  },
+  {
+    id: 6,
+    name: "Laptop Stand",
+    description: "Ergonomic laptop stand",
+    price: 5,
+    category: "Office",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_D7JKooQO0Ou2qgNguM5W7lXe50Drgnd22g&s"
+  }
+];
+
+export default products;
